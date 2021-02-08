3768b74036e4fb4764b6f11f4de5005ee0972fb5
tamilkrish <tamil.krish45@gmail.com>
2021-02-08 19:45:38 +0530

    first commit

---
+++ GithubEx/src/git/src/Gitexfile.java
@@ -0,0 +1,11 @@
+package git.src;
+
+public class Gitexfile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10,b=20;
+System.out.println(a>b?a:b);
+	}
+
+}
